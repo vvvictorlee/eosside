9cb408ad693cc96096a43818c662408829f43a4d
kingblock <kingblock@163.com>
2018-10-08 18:15:49 +0800

    eos side

--- cmd/eossidecoind/main.go
+++ cmd/eossidecoind/main.go
@@ -23,8 +23,8 @@ func main() {
 	ctx := server.NewDefaultContext()
 
 	rootCmd := &cobra.Command{
-		Use:               "basecoind",
-		Short:             "Basecoin Daemon (server)",
+		Use:               "eossidecoind",
+		Short:             "eossidecoind Daemon (server)",
 		PersistentPreRunE: server.PersistentPreRunEFn(ctx),
 	}
 
@@ -41,36 +41,6 @@ func main() {
 		// Note: Handle with #870
 		panic(err)
 	}
-}
-
-// StartCmd runs the service passed in, either stand-alone or in-process with
-// Tendermint.
-func StartCmd(ctx *Context, appCreator AppCreator) *cobra.Command {
-	cmd := &cobra.Command{
-		Use:   "start",
-		Short: "Run the full node",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			if !viper.GetBool(flagWithTendermint) {
-				ctx.Logger.Info("Starting ABCI without Tendermint")
-				return startStandAlone(ctx, appCreator)
-			}
-
-			ctx.Logger.Info("Starting ABCI with Tendermint")
-
-			_, err := startInProcess(ctx, appCreator)
-			return err
-		},
-	}
-
-	// core flags for the ABCI application
-	cmd.Flags().Bool(flagWithTendermint, true, "Run abci app embedded in-process with tendermint")
-	cmd.Flags().String(flagAddress, "tcp://0.0.0.0:26658", "Listen address")
-	cmd.Flags().String(flagTraceStore, "", "Enable KVStore tracing to an output file")
-	cmd.Flags().String(flagPruning, "syncable", "Pruning strategy: syncable, nothing, everything")
-
-	// add support for all Tendermint-specific command line options
-	tcmd.AddNodeFlags(cmd)
-	return cmd
 }
 
 func newApp(logger log.Logger, db dbm.DB, storeTracer io.Writer) abci.Application {
